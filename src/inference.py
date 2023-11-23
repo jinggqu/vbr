fix loss calculation error and other bugs

src/inference.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 import json
 import logging
-import os.path
+import os
 
 import numpy as np
 import torch
@@ -21,7 +21,7 @@ def inference(folder, inference_data) -> None:
     logger = logging.getLogger(__name__)
     logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     logger.info('config = \n{}'.format(json.dumps(CFG, indent=4)))
-    logger.info('===> Inference started.')
+    logger.info('Inference started.')
 
     # fix random seeds for reproducibility
     np.random.seed(config.train.seed)
@@ -47,10 +47,12 @@ def inference(folder, inference_data) -> None:
 
     with open(os.path.join(folder, 'inference_result.txt'), 'w') as f:
         f.writelines('index, result\n')
+        msg_list = []
         for i, line in enumerate(result):
-            f.writelines(f'{i}, {line}\n')
+            msg_list.append(f'{i}, {line}\n')
+        f.writelines(msg_list)
 
-    logger.info('===> Inference finished.')
+    logger.info('Inference finished.')
 
 
 if __name__ == '__main__':

src/model/model.py
@@ -21,7 +21,10 @@ class LSTM(BaseModel, ABC):
         super().__init__(config)
         self.model = None
         self.optimizer = None
-        self.criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor(self.config.train.loss_weight).to(self.device))
+        self.criterion = nn.CrossEntropyLoss(
+            weight=torch.FloatTensor(self.config.train.loss_weight).to(self.device),
+            reduction='mean'
+        )
 
         self.train_data, self.val_data = {'files': [], 'labels': []}, {'files': [], 'labels': []}
         self.train_loss, self.val_loss = [], []
@@ -44,7 +47,6 @@ class LSTM(BaseModel, ABC):
         self.logger.info(self.model)
 
     def train(self) -> None:
-        best_loss = 100.
         optimizer = torch.optim.Adam(params=self.model.parameters(), lr=self.config.train.lr)
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=self.config.train.gamma)
 
@@ -68,64 +70,88 @@ class LSTM(BaseModel, ABC):
         self.logger.info(f'Length of train sequence: {train_samples_count}')
         self.logger.info(f'Length of validate sequence: {val_samples_count}')
 
-        self.logger.info("===> Training started.")
+        self.logger.info('Training started.')
+
+        highest_acc = 0.
         for epoch in range(self.config.train.epoch):
-            train_loss_tmp = 0.
+            train_loss_sum = 0.
             self.model.train()
-            for step, batch in enumerate(train_dataloader):
+            for batch_index, batch in enumerate(train_dataloader):
                 x, y = batch[0].to(self.device), batch[1].to(self.device)
                 optimizer.zero_grad()
                 output = self.model(x)
                 loss = self.criterion(output, y)
                 loss.backward()
                 optimizer.step()
-                train_loss_tmp += loss.item()
+                train_loss_sum += loss.item()
 
                 prediction = torch.argmax(output, dim=1)
                 acc = (prediction == y).float().mean()
 
                 self.logger.info(
-                    "===> Epoch [{:0>3d}/{:0>3d}] ({:0>3d}/{:0>3d}), Loss : {:.8f}, Accuracy : {:.8f}".format(
-                        epoch + 1, self.config.train.epoch, step + 1, len(train_dataloader), loss.item(), acc
+                    'Epoch [{:0>3d}/{:0>3d}] ({:0>3d}/{:0>3d}), Loss : {:.8f}, Accuracy : {:.8f}'.format(
+                        epoch + 1, self.config.train.epoch, batch_index + 1, len(train_dataloader), loss.item(), acc
                     )
                 )
-            self.train_loss.append(train_loss_tmp / len(train_dataloader))
-            self.logger.info("===> Current Learning Rate: {:.8f}".format(scheduler.get_last_lr()[0]))
+            self.train_loss.append(train_loss_sum / len(train_dataloader))
+            self.logger.info('Current Learning Rate: {:.8f}'.format(scheduler.get_last_lr()[0]))
             scheduler.step()
 
             # Validation
-            val_loss, val_acc = 0., 0.
+            val_loss_sum, val_acc_sum, val_acc_mean = 0., 0., 0.
             self.model.eval()
             with torch.no_grad():
                 for _, batch in enumerate(val_dataloader):
                     x, y = batch[0].to(self.device), batch[1].to(self.device)
                     output = self.model(x)
                     loss = self.criterion(output, y)
-                    val_loss += loss.item()
+                    val_loss_sum += loss.item()
 
                     prediction = torch.argmax(output, dim=1)
-                    val_acc += (prediction == y).sum().item()
+                    val_acc_sum += (prediction == y).sum().item()
 
+                # Get the mean validation acc of current epoch
+                val_acc_mean = val_acc_sum / val_samples_count
                 self.logger.info(
-                    "===> Validation, Average Loss : {:.8f}, Accuracy : {:.8f}".format(
-                        val_loss / val_samples_count, val_acc / val_samples_count
+                    'Validation, Average Loss : {:.8f}, Accuracy : {:.8f}'.format(
+                        val_loss_sum / len(val_dataloader), val_acc_mean
                     )
                 )
 
-            self.val_loss.append(val_loss / len(val_dataloader))
+            self.val_loss.append(val_loss_sum / len(val_dataloader))
 
-            # Save best model
-            if best_loss > self.val_loss[-1]:
-                best_loss = self.val_loss[-1]
-                self.save(epoch, self.model)
+            # Log loss
+            self.log_loss(epoch)
 
-        self.logger.info("===> Training finished.")
+            # Save the best model
+            if highest_acc < val_acc_mean:
+                self.logger.info('Highest accuracy was updated from {:.8f} to {:.8f}'.format(
+                    highest_acc, val_acc_mean)
+                )
+                highest_acc = val_acc_mean
+                self.save_model(self.model)
 
-    def save(self, epoch: int, model: torch.nn.Module) -> None:
-        model_path = os.path.join(self.save_to, 'best_model.pth')
-        jit_model_path = os.path.join(self.save_to, 'best_model.pt')
+        self.logger.info('Training finished.')
+
+    def log_loss(self, epoch: int) -> None:
         loss_path = os.path.join(self.save_to, 'loss.log')
         fig_path = os.path.join(self.save_to, 'loss.png')
+
+        loss = np.column_stack((self.train_loss, self.val_loss))
+        np.savetxt(loss_path, loss, fmt='%.8f', delimiter=',')
+
+        plt.gcf().set_size_inches(8, 6)
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.plot(np.linspace(0, epoch, epoch + 1).tolist(), self.train_loss, label='train loss')
+        plt.plot(np.linspace(0, epoch, epoch + 1).tolist(), self.val_loss, label='val loss')
+        plt.legend(['train loss', 'val loss'])
+        plt.savefig(fig_path, bbox_inches='tight', dpi=600)
+        plt.clf()
+
+    def save_model(self, model: torch.nn.Module) -> None:
+        model_path = os.path.join(self.save_to, 'best_model.pth')
+        jit_model_path = os.path.join(self.save_to, 'best_model.pt')
 
         # Save the whole model for continuous training
         torch.save(model, model_path)
@@ -139,17 +165,4 @@ class LSTM(BaseModel, ABC):
         traced_model = torch.jit.trace(script_model,
                                        torch.rand(1, 1, self.config.model.input_size))
         traced_model.save(jit_model_path)
-
-        loss = np.column_stack((self.train_loss, self.val_loss))
-        np.savetxt(loss_path, loss, fmt='%.8f', delimiter=',')
-
-        plt.clf()
-        plt.gcf().set_size_inches(8, 6)
-        plt.xlabel('epoch')
-        plt.ylabel('loss')
-        plt.plot(np.linspace(0, epoch, epoch + 1).tolist(), self.train_loss, label='train loss')
-        plt.plot(np.linspace(0, epoch, epoch + 1).tolist(), self.val_loss, label='val loss')
-        plt.legend(['train loss', 'val loss'])
-        plt.savefig(fig_path, bbox_inches='tight', dpi=300)
-        plt.clf()
-        self.logger.info(f'===> Model saved to {model_path}')
+        self.logger.info(f'Model saved to {model_path}')

src/process_data.py
@@ -13,24 +13,29 @@ config = Config.from_json(CFG)
 
 def split_data():
     root_dir = config.root_dir
-    raw_data_dir = config.data.raw_data
+    raw_data_dir_list = config.data.raw_data_list
 
     # Raw data column list:
     # 'ts', 'longitude', 'latitude', 'tgtheight'(always 0), 'course', 'speed', 'fusiontype'(2 or 11 or 1 or 13), 
     # 'srcmmsi', 'isconnecting'(always 0), 'connecttgttype'(always 0), 'withinborder'(always 0 or 1), 
     # 'starttime', 'batchnum'(unique id), 'tracetype'(always 0 or 1)
 
-    # Load data and group by batchnum
-    data = pd.read_csv(os.path.join(root_dir, raw_data_dir))
-    i, row_count = 0, data['batchnum'].unique().shape[0]
+    print('Raw data splitting started.')
 
-    for batch_id, group in data.groupby('batchnum'):
-        print(f'Complete {i} / {row_count}', end='\r')
-        # Get same batch id data and save them to a single csv file
-        group.to_csv(os.path.join(root_dir, config.data.raw_folder, f'{batch_id}.csv'), index=False)
-        i += 1
+    for raw_data_dir in raw_data_dir_list:
+        print(f'\nSplitting raw data file: {raw_data_dir}')
 
-    print("\nRaw data splitting completed.", end=None)
+        # Load data and group by batchnum
+        data = pd.read_csv(os.path.join(root_dir, raw_data_dir))
+        i, row_count = 0, data['batchnum'].unique().shape[0]
+
+        for batch_id, group in data.groupby('batchnum'):
+            print(f'Splitted {i} / {row_count}', end='\r')
+            # Get same batch id data and save them to a single csv file
+            group.to_csv(os.path.join(root_dir, config.data.raw_folder, f'{batch_id}.csv'), index=False)
+            i += 1
+
+    print('\nRaw data splitting completed.')
 
 
 def preprocess():
@@ -42,7 +47,9 @@ def preprocess():
     classes = set([])
 
     # Need to be kept column list and type:
-    # 'ts', 'longitude', 'latitude', 'course', 'speed', 'fusiontype'(2 or 11 or 1 or 13),
+    # 'ts', 'longitude', 'latitude', 'course', 'speed', 'fusiontype'(2 or 11 or 1 or 13), withinborder
+
+    print('\nData preprocessing started.')
 
     i, count = 1, len(files)
     for file in files:
@@ -55,8 +62,9 @@ def preprocess():
         classes.add(df['tracetype'][0])
 
         # Drop useless columns
-        df.drop(['tgtheight', 'srcmmsi', 'isconnecting', 'connecttgttype', 'withinborder',
-                 'starttime', 'batchnum', 'tracetype'], axis=1, inplace=True)
+        df.drop(labels=['tgtheight', 'course', 'srcmmsi', 'isconnecting',
+                        'connecttgttype', 'starttime', 'batchnum', 'tracetype'],
+                axis=1, inplace=True)
 
         # Split datetime to day of the year and second of the day, then normalize them
         df_time = pd.to_datetime(df['ts'])
@@ -67,7 +75,6 @@ def preprocess():
         # Normalize other variables
         df['longitude'] = df['longitude'] / 180.  # West to East (-180° to 180°)
         df['latitude'] = df['latitude'] / 90.  # South to North (-90° to 90°)
-        df['course'] = df['course'] / 360.  # Direction (0° to 360°)
         df['speed'] = df['speed'] / 80.  # Knots (0 to 80)
         df['fusiontype'] = df['fusiontype'].map({1: 0, 2: 1, 11: 2, 13: 3}) / 3.
 
@@ -82,7 +89,7 @@ def preprocess():
     with open(os.path.join(root_dir, config.data.proc_folder, 'label_classes.txt'), 'wb') as f:
         np.savetxt(f, list(classes), fmt='%d')
 
-    print("\nData preprocessing completed.")
+    print('\nData preprocessing completed.')
 
 
 if __name__ == '__main__':

src/test.py
@@ -25,7 +25,7 @@ def test(folder) -> None:
     logger = logging.getLogger(__name__)
     logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     logger.info('config = \n{}'.format(json.dumps(CFG, indent=4)))
-    logger.info('===> Testing started.')
+    logger.info('Testing started.')
 
     # fix random seeds for reproducibility
     np.random.seed(config.train.seed)
@@ -58,11 +58,22 @@ def test(folder) -> None:
             x, y = batch[0].to(device), batch[1].to(device)
             y_true += y.tolist()
             output = model(x)
-            prediction = torch.argmax(output, dim=1)
-            y_pred += prediction.tolist()
+            y_pred += torch.argmax(output, dim=1).tolist()
 
     with open(os.path.join(folder, 'test_result.txt'), 'w') as f:
         f.writelines('\n'.join(str(line) for line in y_pred))
+
+    # Save wrong prediction files' name
+    msg_list = []
+    with open(os.path.join(folder, 'test_wrong_files.txt'), 'w') as f:
+        for i in range(len(y_true)):
+            if y_pred[i] == 1 and y_true[i] == 0:
+                msg_list.append(f'FP (False Positive): {test_dataloader.dataset.files[i]}, \t'
+                                f'true label: 0, predicted label: 1\n')
+            elif y_pred[i] == 0 and y_true[i] == 1:
+                msg_list.append(f'FN (False Negative): {test_dataloader.dataset.files[i]}, \t'
+                                f'true label: 1, predicted label: 0\n')
+        f.writelines(msg_list)
 
     classes = data_loader.label_classes()
     matrix = metrics.confusion_matrix(y_true, y_pred, labels=classes.argsort())
@@ -79,7 +90,7 @@ def test(folder) -> None:
                     'precision:\t' + str(metrics.precision_score(y_true, y_pred, average=average)),
                     'recall: \t' + str(metrics.recall_score(y_true, y_pred, average=average)),
                     'f1_score:\t' + str(metrics.f1_score(y_true, y_pred, average=average)),
-                    'auc:\t' + str(metrics.roc_auc_score(y_true, y_pred, average=average))]
+                    'auc:\t\t' + str(metrics.roc_auc_score(y_true, y_pred, average=average))]
 
     with open(os.path.join(folder, 'test_metrics.txt'), 'w') as f:
         f.writelines('\n'.join(test_metrics))
@@ -87,7 +98,7 @@ def test(folder) -> None:
     fpr, tpr, _ = metrics.roc_curve(y_true, y_pred)
     plot_roc(fpr=fpr, tpr=tpr, save_to=os.path.join(folder, 'roc_curve.png'), ticks_rotation=0, show=False)
 
-    logger.info('===> Testing finished.')
+    logger.info('Testing finished.')
 
 
 if __name__ == '__main__':