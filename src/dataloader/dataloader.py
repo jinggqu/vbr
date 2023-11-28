325cb743d2fbf6101ad0bf5ac2344365d2c3fdbb
sudrizzz <sudrizzz@gmail.com>
2023-11-28 10:11:38 +0800

    docs: add documentation to dataloader related code lines

--- src/dataloader/dataloader.py
+++ src/dataloader/dataloader.py
@@ -12,7 +12,10 @@ class DataLoader:
     def load_data(self):
         with open(os.path.join(self.config.root_dir, self.config.data.proc_folder, 'data.json'), 'r') as f:
             data = json.load(f)
-        files, labels = np.array(list(data.keys())), np.array(list(data.values()))
+
+        # Set the file name type as object to prevent string data truncation by numpy
+        files, labels = np.array(list(data.keys()), dtype=object), np.array(list(data.values()))
+
         return self.train_val_test_split(files=files,
                                          labels=labels,
                                          train_portion=self.config.train.train_portion,
@@ -29,12 +32,18 @@ class DataLoader:
         Split dataset into train, val and test dataset
         """
         if shuffle:
+            # Fix random seeds for reproducibility
+            # Ensure that seed() and permutation() appear in pairs at the same time
             np.random.seed(self.config.train.seed)
             permutation = np.random.permutation(len(files))
-            np.savetxt(os.path.join(self.config.root_dir, self.config.data.proc_folder, 'permutation.txt'),
-                       permutation, fmt='%d')
+
+            # Randomize the files and labels in the same order
             files = files[permutation]
             labels = labels[permutation]
+
+            # Save permutation sequence to a text file
+            np.savetxt(os.path.join(self.config.root_dir, self.config.data.proc_folder, 'permutation.txt'),
+                       permutation, fmt='%d')
 
         train_size = int(len(files) * train_portion)
         val_size = int(len(files) * val_portion)
@@ -55,6 +64,10 @@ class DataLoader:
         }
 
     def label_classes(self):
+        """
+        Load all classes and encode them by using LabelEncoder()
+        :return:
+        """
         label_encoder = LabelEncoder()
         label_encoder.classes_ = np.loadtxt(
             fname=os.path.join(self.config.root_dir, self.config.data.proc_folder, 'label_classes.txt'),
